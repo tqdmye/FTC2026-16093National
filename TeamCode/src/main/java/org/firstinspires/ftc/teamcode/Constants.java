2b6f4908a6a733adc36bc802cf89499e1aec5e5a
OleslieO <leslietan0521@gmail.com>
2025-12-09 21:42:44 +0800

    color sensor testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Constants {
+    @Config
+    public static class ColorConstants{
+        public static double DIS_BOUNDARY = 2.2;
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testColor.java
@@ -1,31 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.ArrayList;
 
 @TeleOp(name="colortest")
 public class testColor extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class,"leftFront");
-        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class,"leftRear");
-        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class,"rightFront");
-        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class,"rightRear");
+        DcMotorEx leftFront = hardwareMap.get(DcMotorEx.class,"leftFront");
+        DcMotorEx leftRear = hardwareMap.get(DcMotorEx.class,"leftRear");
+        DcMotorEx rightFront = hardwareMap.get(DcMotorEx.class,"rightFront");
+        DcMotorEx rightRear = hardwareMap.get(DcMotorEx.class,"rightRear");
         RevColorSensorV3 revColorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
 
 
         revColorSensor.enableLed(true);
 
-        frontRightMotor.setDirection(DcMotorEx.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        rightRear.setDirection(DcMotorEx.Direction.REVERSE);
 
-        int[] colorRecord = new int[3];
-        int colorIndex = 0;
+
+        ArrayList<Integer> colorRecord = new ArrayList<>();
+
         boolean inZone = false;
 
 
@@ -38,10 +42,10 @@ public class testColor extends LinearOpMode {
 
 
             double a = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(rx), 1);
-            frontLeftMotor.setPower((y + x + rx) / a);
-            backLeftMotor.setPower((y - x + rx) / a);
-            frontRightMotor.setPower((y - x - rx) / a);
-            backRightMotor.setPower((y + x - rx) / a);
+            leftFront.setPower((y + x + rx) / a);
+            leftRear.setPower((y - x + rx) / a);
+            rightFront.setPower((y - x - rx) / a);
+            rightRear.setPower((y + x - rx) / a);
             double r = revColorSensor.red();
             double g = revColorSensor.green();
             double b = revColorSensor.blue();
@@ -49,21 +53,17 @@ public class testColor extends LinearOpMode {
             boolean isPurple = (r + b) / 2 > g && r > 80 && b > 80;
 
             double distance = ((DistanceSensor) revColorSensor).getDistance(DistanceUnit.CM);
-            if(distance <=2.2 && ! inZone){
-                if(colorIndex < 3) {
-                    if (isPurple) {
-                        colorRecord[colorIndex] = 1;
-                    }
-                    else if (isGreen) {
-                        colorRecord[colorIndex] = 0;
-                    } else {
+            if (distance <= Constants.ColorConstants.DIS_BOUNDARY && !inZone) {
 
-                    }
-                    colorIndex++;
+                if (isPurple) {
+                    colorRecord.add(1);   // 紫色
+                } else if (isGreen) {
+                    colorRecord.add(0);   // 绿色
                 }
                 inZone = true;
             }
-            if (distance > 2.2){
+            //2.2cm以内允许检测并记录，紫色记录为1，绿色记录为0
+            if (distance > Constants.ColorConstants.DIS_BOUNDARY){
                 inZone =  false;
             }
 
@@ -75,10 +75,9 @@ public class testColor extends LinearOpMode {
             telemetry.addData("Detected PURPLE", isPurple);
             telemetry.addData("Distance", distance);
             telemetry.addLine("--------ColorRecord--------");
-            telemetry.addData("0", colorRecord[0]);
-            telemetry.addData("1", colorRecord[1]);
-            telemetry.addData("2", colorRecord[2]);
-            telemetry.addData("Index", colorIndex);
+            telemetry.addData("ListSize", colorRecord.size());
+            telemetry.addData("Record", colorRecord.toString());
+            telemetry.addData("InZone", inZone);
             telemetry.addData("InZone", inZone);
             telemetry.update();
         }
